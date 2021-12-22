5662626865cc2563de8b0bfc94a86df1bec80d6e
Abhishek Kumar <abhishek160.kumar@ril.com>
2021-12-22 10:46:26 +0530

    feat: Add test display name

--- src/test/java/com/learn/unit/test/spirngunittest/controllers/IndexControllerTest.java
+++ src/test/java/com/learn/unit/test/spirngunittest/controllers/IndexControllerTest.java
@@ -1,6 +1,7 @@
 package com.learn.unit.test.spirngunittest.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,11 +15,13 @@ class IndexControllerTest {
         controller = new IndexController();
     }
 
+    @DisplayName("test to verify the index view name")
     @Test
     void index() {
         assertEquals("index", controller.index(), "wrong view returned");
     }
 
+    @DisplayName("test to verify the exception")
     @Test
     void oupsHandler() {
         // thi assertion is used for test boolean condition
